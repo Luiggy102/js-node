455e284589d2c1ae624b5882275c6aede31a13a9
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2022-12-12 13:46:45 -0500

    callbacks in node

--- node/async/callback.js
+++ node/async/callback.js
@@ -1,1 +1,40 @@
-// 
+//  ================== Callbacks ================= //
+// funciones como parametros
+
+//function IamAsync(miCallback) {
+
+    //setTimeout(() => {
+        //console.log(' === funcion asincrona === ');
+        //console.log(' funciones como parametros');
+        //miCallback();
+    //}, 1000);
+
+//}
+
+//console.log('\n iniciando proceso...');
+
+// se le pasa una funcion para cuando acabe el proceso
+//IamAsync(() => console.log(' \n terminado proceso...'));
+
+// ============ Ejemplo 2 =========== //
+console.clear();
+
+function saludar(nombre, callback) {
+
+    setTimeout(() => {
+        console.log('hola ' + nombre);
+        callback();
+    }, 1000);
+
+};
+
+function despedir(nombre, otroCallback) {
+
+    setTimeout(() => {
+        console.log('Adios ' + nombre)
+        otroCallback();
+    }, 1500);
+
+};
+
+console.log(' \n iniciando proceso 2 ...');
