aae9c4e85c35a22692720b0bee3ae8c0e6e7b2dc
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2022-12-23 12:40:14 -0500

    promises js

--- node/async/promises.js
+++ node/async/promises.js
@@ -12,26 +12,39 @@
 function hello(name) {
     return new Promise(function(resolve) {
         setTimeout(() => {
-            console.log('Hola' + name);
+            console.log(`Hola ${name}`);
             resolve(name)
-        }, 1000);
+        }, 100);
     });
 }
 
-function speak(speakCallback) {
-    setTimeout(() => {
-        console.log('bla, bla, bla');
-        speakCallback();
-    }, 1000);
+function speak(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('bla, bla, bla');
+            resolve(name);
+            reject('Hay un error en la función speak');
+        }, 100);
+    })
 }
 
-function bye(name, anotherCallback) {
-    setTimeout(() => {
-        console.log('Adios ' + name);
-        anotherCallback(); 
-    }, 1000);
+function bye(name) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Adios ${name}`);
+            console.log('End Process...');
+            resolve();
+        }, 100);
+    })
 }
 
 // -- Ejecución 
 
 console.log('Starting Process...');
+
+hello('Charles')
+    .then(speak)
+    .then(bye)
+    .catch(error => { // cualquira que tenga en el reject va a salir
+        console.error(error);
+    })
